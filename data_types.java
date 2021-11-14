7d1bf5a9e61fe557cd29ebfb07a5e56f456cad35
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-11-14 09:28:25 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,45 @@
+package data_types;
+
+public class data_types {
+	public static void main(String[] args) {
+		//decler byte data type
+		byte b=1;
+		System.out.println("byte value is:"+b);
+		
+		
+		//decler short value
+		short a=144;
+		System.out.println("short value is:"+a);
+		
+		//integer value
+		int c=1234;
+		System.out.println("integer value:"+c);
+		
+		//long value
+		long s=23242561;
+		System.out.println("long value:"+s);
+		
+		//float value
+		float d=2.666f;
+		System.out.println("float value is:"+d);
+		
+		// double value
+		double g=1.5555443d;
+		System.out.println("double value is:"+g);
+		
+		//char 
+		char i='c';
+		System.out.println("char:"+c);
+		
+		//String 
+		String p="praveen";
+		
+		
+		boolean f=true;
+		System.out.println("boolean value is:"+f);
+		boolean h=false;
+		System.out.println("boolean value is:"+h);
+		
+	}
+
+}
